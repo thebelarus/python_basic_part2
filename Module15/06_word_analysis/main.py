eb2ea2d6b0c4985007da34f437455b23e687ffcf
user74 <housechel@mail.ru>
2021-06-20 18:13:39 +0500

    Lesson 015 06,09 - Completed

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -12,3 +12,6 @@ if __name__ == '__main__':
     word_input = input('Введите слово: ')
     unique_letters_count = count_unique_letters(word_input)
     print(f'Кол-во уникальных букв: {unique_letters_count}')
+
+
+# зачет!
