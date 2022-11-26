ebde28696e276649d43f2be033c74369856b3996
Pavel Radzivinouski <pavel.radzivinouski@gmail.com>
2022-11-26 03:07:28 +0300

    Completed Homework module №24

--- Module24/04_fathers_mothers_kids/main.py
+++ Module24/04_fathers_mothers_kids/main.py
@@ -1,1 +1,66 @@
-# TODO здесь писать код
+class Child:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.is_hungry = True
+        self.is_calm = True
+
+    def get_about_me(self):
+        return (
+            f'My name {self.name}, i\'m {self.age} years old, '
+            f'is hungry: {self.is_hungry}, is calm: {self.is_calm}')
+
+    def __str__(self):
+        return self.get_about_me()
+
+
+class Parent:
+    def __init__(self, name, age, children=[]):
+        self.name = name
+        self.age = age
+        self.children = []
+        if self.is_valid_inital_children_array(children):
+            self.children = children
+
+    def is_valid_inital_children_array(self, children):
+        return all([
+            child for child in children
+            if self.is_child_valid_for_me(child)]
+            )
+
+    def is_child_valid_for_me(self, child):
+        return self.age - child.age > 16
+
+    def get_about_me(self):
+        children_info = '\n\t'.join(
+            [child.get_about_me() for child in self.children])
+        return (
+            f'My name {self.name}, i\'m {self.age} years '
+            f'old, my children: \n\t{children_info}')
+
+    def add_child(self, child):
+        if self.is_child_valid_for_me(child):
+            self.children.append(child)
+
+    def feed_child(self, child: Child):
+        if child.is_hungry:
+            child.is_hungry = False
+
+    def calm_child(self, child: Child):
+        if child.is_calm:
+            child.is_calm = False
+
+
+def main():
+    child_1 = Child('Juzy', 10)
+    child_2 = Child('Samantha', 8)
+    child_3 = Child('Nicole', 6)
+    parent = Parent('Rick', 30, [child_1, child_2, child_3])
+    print(parent.get_about_me())
+    parent.feed_child(child_1)
+    parent.feed_child(child_2)
+    print(parent.get_about_me())
+
+
+if __name__ == '__main__':
+    main()
--- Module24/07_cohabitation/main.py
+++ Module24/07_cohabitation/main.py
@@ -1,1 +1,80 @@
-# TODO здесь писать код
+import random
+
+
+class House:
+    def __init__(self, fridge=50, money=0):
+        self.fridge = 50
+        self.money = 0
+
+
+class Man:
+    def __init__(self, name, house, health=50):
+        self.name = name
+        self.house = house
+        self.health = health
+
+    def eat(self):
+        if self.health == 0:
+            raise ValueError(
+                f'Man {self.name}, not enough food in fridge for eat!')
+        self.house.fridge -= 1
+        self.health += 1
+
+    def work(self):
+        if self.health == 0:
+            raise ValueError(f'Man {self.name}, not enough health for work!')
+        self.health -= 1
+        self.house.money += 1
+
+    def play(self):
+        if self.health == 0:
+            raise ValueError(f'Man {self.name}, not enough health for play!')
+        self.health -= 1
+
+    def shopping(self):
+        if self.house.money == 0:
+            raise ValueError(
+                f'Man {self.name}, not enough money for shopping!')
+        self.house.money -= 1
+        self.house.fridge += 1
+
+    def simulate_day(self):
+        random_num = random.randint(1, 6)
+        if self.health < 0:
+            raise ValueError(f'Man {self.name} is dead')
+        elif self.health < 20:
+            self.eat()
+        elif self.house.fridge < 10:
+            self.shopping()
+        elif self.house.money < 50:
+            self.work()
+        elif random_num == 1:
+            self.work()
+        elif random_num == 2:
+            self.eat()
+        else:
+            self.play()
+
+
+def main():
+    days_count = 365
+    sweet_house = House()
+    people = (
+            Man('John', sweet_house),
+            Man('Ann', sweet_house),
+            Man('Bob', sweet_house),
+            Man('Jennie', sweet_house),
+        )
+    for _ in range(days_count):
+        for man in people:
+            man.simulate_day()
+    print(f'Stats ater {days_count} days')
+    for man in people:
+        print(f'Name: {man.name}')
+        print(f'\tHealth: {man.health}')
+        print(f'\tFridge: {man.house.fridge}')
+        print(f'\tMoney: {man.house.money}')
+
+
+if __name__ == '__main__':
+    main()
