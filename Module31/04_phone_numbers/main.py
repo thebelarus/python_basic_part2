f0815a11932c178b20b67a69f747ec850b514543
Pavel Radzivinouski <pavel.radzivinouski@gmail.com>
2022-12-16 15:48:05 +0300

    Completed Homework module №31

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+import re
+
+REGEX_PHONE = r'[89]{1}\d{9}'
+
+
+def is_valide_phone(number_string: str) -> bool:
+    result = re.match(REGEX_PHONE, number_string)
+    if result:
+        return True
+    return False
+
+
+def main() -> None:
+    '''Tестирование регулярного выражения'''
+    print('Результат:')
+    phones = ['9999999999', '999999-999', '99999x9999']
+    for phone_number in phones:
+        print(f'номер {phone_number}: ', end='')
+        if is_valide_phone(phone_number):
+            print('всё в порядке')
+        else:
+            print('не подходит')
+
+
+if __name__ == '__main__':
+    main()
--- Module31/05_web_scrapping/main.py
+++ Module31/05_web_scrapping/main.py
@@ -1,1 +1,39 @@
-# TODO здесь писать код
+import re
+import requests
+from typing import List, Tuple
+
+
+def get_page(url: str) -> Tuple[bool, str]:
+    '''Функция загрузки веб-страницы и возврата результат в виде текста.'''
+    try:
+        response = requests.get(url)
+        return True, response.text
+    except requests.ConnectionError:
+        return False, ''
+
+
+def parse_page(page: str) -> List[str]:
+    '''Функция парсинга текса веб-страницы на наличие
+    HTML заголовков 3-го уровня и возврат их в виде массива.'''
+    regex_expression = r'<h3 id=\"\w+\">(.+)<\/h3>'
+    results: List[str] = re.findall(regex_expression, page)
+    return results
+
+
+def main() -> None:
+    '''Tестирование приложения'''
+    print('Результат:')
+    url = 'http://www.columbia.edu/~fdc/sample.html'
+    get_status, page_data = get_page(url)
+    if get_status:
+        headers = parse_page(page_data)
+        for header in headers:
+            print(header)
+    else:
+        print('Не удалось загрузить страницу {}'.format(
+            url
+        ))
+
+
+if __name__ == '__main__':
+    main()
